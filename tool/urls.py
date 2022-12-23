ad20078145445a4d76c143ba1b8da9a3bcf8cb3b
LucoMoro <morelli.2000luca@gmail.com>
2022-12-23 17:03:09 +0100

    Adding inpage style & css to tool and results

--- tool/urls.py
+++ tool/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     path('results', views.results, name='results'),
     path('jupyter', views.jupyter, name='jupyter'),
     path('navtest', views.navtest, name='navtest'),
+    path('footertest', views.footertest, name='footertest'),
 ]
--- tool/views.py
+++ tool/views.py
@@ -19,6 +19,16 @@ def results(request):
     log_interval="1"
     device="cpu"
     dataset="new_york"
+    tessellation_area="new_york"
+    tessellation_size="25000"
+    tile_id_column="tile_ID"
+    tile_geometry="geometry"
+    oa_id_column="GEOID"
+    oa_geometry="geometry"
+    flow_origin_column="geoid_o"
+    flow_destination_column="geoid_d"
+    flow_flows_column="pop_flows"
+
 
     commands = cmdCommands.objects.all()
     for command in commands:
@@ -43,9 +53,29 @@ def results(request):
         if(command.name == "device"):
             device = command.command
         if(command.name == "dataset"):
-            dataset = command.command        
+            dataset = command.command   
+        if(command.name == "tessellation_area"):
+            tessellation_area = command.command 
+        if(command.name == "tessellation_size"):
+            tessellation_size = command.command 
+        if(command.name == "tile_id_column"):
+            tile_id_column = command.command 
+        if(command.name == "tile_geometry"):
+            tile_geometry = command.command 
+        if(command.name == "oa_id_column"):
+            oa_id_column = command.command 
+        if(command.name == "oa_geometry"):
+            oa_geometry = command.command 
+        if(command.name == "flow_origin_column"):
+            flow_origin_column = command.command 
+        if(command.name == "flow_destination_column"):
+            flow_destination_column = command.command 
+        if(command.name == "flow_flows_column"):
+            flow_flows_column = command.command 
+    
 
-    cmd_command="python main.py --dataset "+dataset+" --oa-id-column GEOID --flow-origin-column geoid_o --flow-destination-column geoid_d --flow-flows-column pop_flows --epochs "+epochs+" --batch_size "+batch_size+" --test-batch-size "+test_batch_size+" --lr "+lr+" --momentum "+momentum+" --seed "+seed+" --log-interval "+log_interval+" --device "+device+" --mode "+mode
+    cmd_command="python main.py --dataset "+dataset+" --oa-id-column "+oa_id_column+" --flow-origin-column "+flow_origin_column+" --flow-destination-column "+flow_destination_column+" --flow-flows-column "+flow_flows_column+" --epochs "+epochs+" --batch_size "+batch_size+" --test-batch-size "+test_batch_size+" --lr "+lr+" --momentum "+momentum+" --seed "+seed+" --log-interval "+log_interval+" --device "+device+" --tessellation-area "+tessellation_area+" --tessellation-size "+tessellation_size+" --tile-id-column "+tile_id_column+" --tile-geometry "+tile_geometry+" --oa-geometry "+oa_geometry+" --mode "+mode
+    
     os.chdir(main_path)
     subprocess.run(cmd_command, shell=True, check=True)
     data='Tutto deep_gravity andato a buon fine'
@@ -66,3 +96,6 @@ def jupyter(request):
 
 def navtest(request):
     return render(request, 'tool/navtest.html')
+
+def footertest(request):
+    return render(request, 'tool/footertest.html')
