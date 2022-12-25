78f2c382b7365867e506c841012f8d64bcdaa5ea
LucoMoro <morelli.2000luca@gmail.com>
2022-12-25 12:50:09 +0100

    Adding results pages

--- tool/urls.py
+++ tool/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns=[
     path('', views.tool, name='home'),
-    path('results', views.results, name='results'),
+    path('dg_results', views.dg_results, name='dg_results'),
+    path('g_results', views.g_results, name='g_results'),
+    path('ng_results', views.ng_results, name='ng_results'),
+    path('mfg_results', views.mfg_results, name='mfg_results'),
     path('jupyter', views.jupyter, name='jupyter'),
     path('navtest', views.navtest, name='navtest'),
     path('footertest', views.footertest, name='footertest'),
--- tool/views.py
+++ tool/views.py
@@ -3,91 +3,112 @@ from .models import cmdCommands
 import os
 import subprocess
 
+
+mode="train"
+batch_size="1"
+test_batch_size="1"
+epochs="1"
+lr="5e-6"
+momentum="0.9"
+seed="1234"
+log_interval="1"
+device="cpu"
+dataset="new_york"
+tessellation_area="new_york"
+tessellation_size="25000"
+tile_id_column="tile_ID"
+tile_geometry="geometry"
+oa_id_column="GEOID"
+oa_geometry="geometry"
+flow_origin_column="geoid_o"
+flow_destination_column="geoid_d"
+flow_flows_column="pop_flows"
+
+commands = cmdCommands.objects.all()
+for command in commands:
+    if(command.name == "main_path"):
+        main_path = command.command
+    if(command.name == "mode"):
+        mode = command.command
+    if(command.name == "batch_size"):
+        batch_size = command.command
+    if(command.name == "test_batch_size"):
+        test_batch_size = command.command
+    if(command.name == "epochs"):
+        epochs = command.command
+    if(command.name == "lr"):
+        lr = command.command
+    if(command.name == "momentum"):
+        momentum = command.command
+    if(command.name == "seed"):
+        seed = command.command
+    if(command.name == "log_interval"):
+        log_interval = command.command
+    if(command.name == "device"):
+        device = command.command
+    if(command.name == "dataset"):
+        dataset = command.command   
+    if(command.name == "tessellation_area"):
+        tessellation_area = command.command 
+    if(command.name == "tessellation_size"):
+        tessellation_size = command.command 
+    if(command.name == "tile_id_column"):
+        tile_id_column = command.command 
+    if(command.name == "tile_geometry"):
+        tile_geometry = command.command 
+    if(command.name == "oa_id_column"):
+        oa_id_column = command.command 
+    if(command.name == "oa_geometry"):
+        oa_geometry = command.command 
+    if(command.name == "flow_origin_column"):
+        flow_origin_column = command.command 
+    if(command.name == "flow_destination_column"):
+        flow_destination_column = command.command 
+    if(command.name == "flow_flows_column"):
+        flow_flows_column = command.command 
+    if(command.name == "plots_path"):
+        jupyter_path = command.command
+
 #main html page
 def tool(request):
     return render(request, 'tool/tool.html')
 
 #html page to execute DeepGravity
-def results(request):
-    mode="train"
-    batch_size="1"
-    test_batch_size="1"
-    epochs="1"
-    lr="5e-6"
-    momentum="0.9"
-    seed="1234"
-    log_interval="1"
-    device="cpu"
-    dataset="new_york"
-    tessellation_area="new_york"
-    tessellation_size="25000"
-    tile_id_column="tile_ID"
-    tile_geometry="geometry"
-    oa_id_column="GEOID"
-    oa_geometry="geometry"
-    flow_origin_column="geoid_o"
-    flow_destination_column="geoid_d"
-    flow_flows_column="pop_flows"
-
-
-    commands = cmdCommands.objects.all()
-    for command in commands:
-        if(command.name == "main_path"):
-            main_path = command.command
-        if(command.name == "mode"):
-            mode = command.command
-        if(command.name == "batch_size"):
-            batch_size = command.command
-        if(command.name == "test_batch_size"):
-            test_batch_size = command.command
-        if(command.name == "epochs"):
-            epochs = command.command
-        if(command.name == "lr"):
-            lr = command.command
-        if(command.name == "momentum"):
-            momentum = command.command
-        if(command.name == "seed"):
-            seed = command.command
-        if(command.name == "log_interval"):
-            log_interval = command.command
-        if(command.name == "device"):
-            device = command.command
-        if(command.name == "dataset"):
-            dataset = command.command   
-        if(command.name == "tessellation_area"):
-            tessellation_area = command.command 
-        if(command.name == "tessellation_size"):
-            tessellation_size = command.command 
-        if(command.name == "tile_id_column"):
-            tile_id_column = command.command 
-        if(command.name == "tile_geometry"):
-            tile_geometry = command.command 
-        if(command.name == "oa_id_column"):
-            oa_id_column = command.command 
-        if(command.name == "oa_geometry"):
-            oa_geometry = command.command 
-        if(command.name == "flow_origin_column"):
-            flow_origin_column = command.command 
-        if(command.name == "flow_destination_column"):
-            flow_destination_column = command.command 
-        if(command.name == "flow_flows_column"):
-            flow_flows_column = command.command 
-    
-
-    cmd_command="python main.py --dataset "+dataset+" --oa-id-column "+oa_id_column+" --flow-origin-column "+flow_origin_column+" --flow-destination-column "+flow_destination_column+" --flow-flows-column "+flow_flows_column+" --epochs "+epochs+" --batch_size "+batch_size+" --test-batch-size "+test_batch_size+" --lr "+lr+" --momentum "+momentum+" --seed "+seed+" --log-interval "+log_interval+" --device "+device+" --tessellation-area "+tessellation_area+" --tessellation-size "+tessellation_size+" --tile-id-column "+tile_id_column+" --tile-geometry "+tile_geometry+" --oa-geometry "+oa_geometry+" --mode "+mode
+def dg_results(request):
+    cmd_command="python DG_main.py --dataset "+dataset+" --oa-id-column "+oa_id_column+" --flow-origin-column "+flow_origin_column+" --flow-destination-column "+flow_destination_column+" --flow-flows-column "+flow_flows_column+" --epochs "+epochs+" --batch_size "+batch_size+" --test-batch-size "+test_batch_size+" --lr "+lr+" --momentum "+momentum+" --seed "+seed+" --log-interval "+log_interval+" --device "+device+" --tessellation-area "+tessellation_area+" --tessellation-size "+tessellation_size+" --tile-id-column "+tile_id_column+" --tile-geometry "+tile_geometry+" --oa-geometry "+oa_geometry+" --mode "+mode
     
     os.chdir(main_path)
     subprocess.run(cmd_command, shell=True, check=True)
-    data='Tutto deep_gravity andato a buon fine'
-    return render(request, 'tool/results.html', {'data': data})
+    data='DG eseguito con successo'
+    return render(request, 'tool/dg_results.html', {'data': data})
+
+def g_results(request):
+    cmd_command="python G_main.py --dataset "+dataset+" --oa-id-column "+oa_id_column+" --flow-origin-column "+flow_origin_column+" --flow-destination-column "+flow_destination_column+" --flow-flows-column "+flow_flows_column+" --epochs "+epochs+" --batch_size "+batch_size+" --test-batch-size "+test_batch_size+" --lr "+lr+" --momentum "+momentum+" --seed "+seed+" --log-interval "+log_interval+" --device "+device+" --tessellation-area "+tessellation_area+" --tessellation-size "+tessellation_size+" --tile-id-column "+tile_id_column+" --tile-geometry "+tile_geometry+" --oa-geometry "+oa_geometry+" --mode "+mode
+    
+    os.chdir(main_path)
+    subprocess.run(cmd_command, shell=True, check=True)
+    data='G eseguito con successo'
+    return render(request, 'tool/g_results.html', {'data': data})
+
+def ng_results(request):
+    cmd_command="python NG_main.py --dataset "+dataset+" --oa-id-column "+oa_id_column+" --flow-origin-column "+flow_origin_column+" --flow-destination-column "+flow_destination_column+" --flow-flows-column "+flow_flows_column+" --epochs "+epochs+" --batch_size "+batch_size+" --test-batch-size "+test_batch_size+" --lr "+lr+" --momentum "+momentum+" --seed "+seed+" --log-interval "+log_interval+" --device "+device+" --tessellation-area "+tessellation_area+" --tessellation-size "+tessellation_size+" --tile-id-column "+tile_id_column+" --tile-geometry "+tile_geometry+" --oa-geometry "+oa_geometry+" --mode "+mode
+    
+    os.chdir(main_path)
+    subprocess.run(cmd_command, shell=True, check=True)
+    data='NG eseguito con successo'
+    return render(request, 'tool/ng_results.html', {'data': data})
+
+def mfg_results(request):
+    cmd_command="python MFG_main.py --dataset "+dataset+" --oa-id-column "+oa_id_column+" --flow-origin-column "+flow_origin_column+" --flow-destination-column "+flow_destination_column+" --flow-flows-column "+flow_flows_column+" --epochs "+epochs+" --batch_size "+batch_size+" --test-batch-size "+test_batch_size+" --lr "+lr+" --momentum "+momentum+" --seed "+seed+" --log-interval "+log_interval+" --device "+device+" --tessellation-area "+tessellation_area+" --tessellation-size "+tessellation_size+" --tile-id-column "+tile_id_column+" --tile-geometry "+tile_geometry+" --oa-geometry "+oa_geometry+" --mode "+mode
+    
+    os.chdir(main_path)
+    subprocess.run(cmd_command, shell=True, check=True)
+    data='MFG eseguito con successo'
+    return render(request, 'tool/mfg_results.html', {'data': data})
 
 
 #html page to render the plots.ipynb file
 def jupyter(request):
-    commands = cmdCommands.objects.all()
-    for command in commands:
-        if(command.name == "plots_path"):
-            jupyter_path = command.command
     os.chdir(jupyter_path)
     subprocess.run("jupyter notebook plots.ipynb", shell=True, check=True)
     data='Tutto jupyter andato a buon fine'
